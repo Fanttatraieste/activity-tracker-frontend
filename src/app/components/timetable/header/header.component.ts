c64f53c6d0d6e6c0dacc67f9c2ff938369e4d569
PrialaRadu <priala.radu@gmail.com>
2026-01-17 20:24:45 +0200

    modificari la Orarul Meu

--- src/app/components/timetable/header/header.component.ts
+++ src/app/components/timetable/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderToolsComponent } from './header-tools/header-tools.component';
 import { HeaderSelectorsComponent } from './header-selectors/header-selectors.component';
+import { AuthService } from '../../../services/auth.service';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -19,6 +21,9 @@ export class HeaderComponent {
   @Input() selectedWeek!: 1 | 2;
   @Input() showWeather: boolean = false;
 
+  private userService = inject(UserService);
+  private authService = inject(AuthService);
+
   // Evenimente agregate care vor fi transmise mai departe catre componenta parinte (App)
   @Output() groupChange = new EventEmitter<number>();
   @Output() specializationChange = new EventEmitter<string>();
@@ -33,7 +38,31 @@ export class HeaderComponent {
 
   // Logica pentru "Orarul Meu": seteaza automat o grupa specifica (ex: 333.1)
   onMyScheduleClicked() {
-    this.groupChange.emit(333.1);
+    const user = this.authService.getCurrentUser();
+    if (user && user.uuid) {
+      this.userService.getProfile(user.uuid).subscribe({
+        next: (profile) => {
+          if (profile && profile.grupa && profile.grupa.trim() !== '') {
+            const digits = profile.grupa.replace(/[^0-9]/g, '');
+
+            if (digits.length >= 4) {
+              const formattedGroup = digits.substring(0, 3) + '.' + digits.substring(3);
+              const groupNum = Number(formattedGroup);
+
+              if (!isNaN(groupNum)) {
+                this.groupChange.emit(groupNum);
+                return;
+              }
+            }
+          }
+
+          this.groupChange.emit(333.1);
+        },
+        error: (err) => {
+          this.groupChange.emit(333.1);
+        }
+      });
+    }
   }
 
   weatherEnabled = false;
